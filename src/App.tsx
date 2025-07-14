a037694d63caefced57bc698cdfba7332fe6e7cd
itgk.j0x <itgk.j0x@gmail.com>
2025-07-14 15:22:51 +0900

    Base

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>MY_Ramen_History</h1>
+    <h2>ラーメンに行ってから
+      <strong>10日</strong>
+        が経過しました</h2>
+    <p>ラーメンを記録します</p>
+    </>
+  )
+}
+
+export default App
