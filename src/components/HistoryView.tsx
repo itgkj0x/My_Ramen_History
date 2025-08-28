8c29aacc8d01da22de09baf08362d05601111b77
itgk.j0x <itgk.j0x@gmail.com>
2025-08-29 01:36:11 +0900

    update

--- src/components/HistoryView.tsx
+++ src/components/HistoryView.tsx
@@ -8,6 +8,18 @@ function HistoryView({
   history: { shop: string; date: string; rating: string }[];
   onDelete: (index: number) => void;
 }) {
+  if (history.length === 0) {
+    return (
+      <div id="cont">
+        <p style={{ color: '#fff', fontSize: '1.3rem', margin: '2em' }}>
+          記録がありません
+          <br />
+          「追加」ボタンから記録を追加しよう！
+        </p>
+      </div>
+    );
+  }
+
   return (
     <>
       <div id="cont">
